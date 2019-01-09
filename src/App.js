ed1e80e6e537dd0f169d52e5e023c20e00b830db
leandro <leandro@isset.nl>
2019-01-09 15:56:37 +0100

    react hooks added

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function Todo({ todo, index}) {
+  return <div className="todo">{todo.text}</div>;
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue (e.target.value)} placeholder="add todo" />
+    </form>
+  )
+}
+
+function App(){
+   const [todos, setTodos] = useState([
+     {
+       text: 'learn about react',
+       isCompleted: false
+     },
+     {
+      text: 'learn php',
+      isCompleted: false
+    },
+    {
+      text: 'learn javascript',
+      isCompleted: false
+    }
+   ]);
+
+   const addTodo = text => {
+     const newTodos = [...todos, {text}];
+     setTodos(newTodos);
+   }
+
+   return (
+     <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo key={index} index={index} todo={todo}/>
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </div>
+     </div>
+   )
+}
+
+export default App;
